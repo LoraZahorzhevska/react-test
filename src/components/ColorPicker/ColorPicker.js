231c53733b4faa2ca36634e8ead37065af2d99b1
LoraZahorzhevska <Zagorzhevskaya@gmail.com>
2021-06-14 10:08:06 +0300

    заканчивает колорпикер

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -3,28 +3,53 @@ import "./ColorPicker.css";
 
 class ColorPicker extends Component {
   state = {
-    activeOptionsIdx: 0,
+    activeOptionsIdx: 3,
+  };
+
+  setActiveIdx = (index) => {
+    this.setState({ activeOptionsIdx: index });
+  };
+
+  makeOptionsClassName = (index) => {
+    const optionClasses = ["ColorPicker__option"];
+
+    if (index === this.state.activeOptionsIdx) {
+      optionClasses.push("ColorPicker__option--active");
+    }
+
+    return optionClasses.join(" ");
   };
 
   render() {
+    // const activeOptionLabel = this.props.options[this.state.activeOptionsIdx];
+    //   вычисляемое свойство - вычисляется на базе проп и стейт
+    const { label } = this.props.options[this.state.activeOptionsIdx];
+
     return (
       <div className="ColorPicker">
         <h2 className="ColorPicker__title">Color Picker</h2>
+        <p>Выбран цвет:{label}</p>
 
         <div>
-          {this.props.options.map(({ label, color }, index) => (
-            <button
-              key={label}
-              className="ColorPicker__option"
-              style={{
-                backgroundColor: color,
-                border:
-                  index === this.state.activeOptionsIdx
-                    ? "5px solid black"
-                    : "none",
-              }}
-            ></button>
-          ))}
+          {this.props.options.map(({ label, color }, index) => {
+            const optionClassName = this.makeOptionsClassName(index);
+
+            return (
+              <button
+                key={label}
+                // className="ColorPicker__option"
+                className={optionClassName}
+                style={{
+                  backgroundColor: color,
+                  //   border:
+                  //     index === this.state.activeOptionsIdx
+                  //       ? "5px solid black"
+                  //       : "none",
+                }}
+                onClick={() => this.setActiveIdx(index)}
+              ></button>
+            );
+          })}
         </div>
       </div>
     );
