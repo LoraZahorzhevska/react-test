a9d8a099223555244d3ed8acf53bcb165d3f08d5
LoraZahorzhevska <Zagorzhevskaya@gmail.com>
2021-06-27 20:26:13 +0300

    добавляет примеры с радио и чекбоксами

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import shortid from "shortid";
 
 class Form extends Component {
   // этот state не для сабмита, а для рендера формы
   state = {
     name: "",
     tag: "",
+    experience: "junior",
+    licence: false,
   };
+
+  nameInputId = shortid.generate();
+  tagInputId = shortid.generate();
 
   handleChange = (event) => {
     // console.log(event.currentTarget);
@@ -39,6 +45,13 @@ class Form extends Component {
   // };
 
   // сбрасывает значение в инпутах
+
+  handleLicenceChange = (event) => {
+    console.log(event.currentTarget.checked);
+
+    this.setState({ licence: event.currentTarget.checked });
+  };
+
   reset = () => {
     this.setState({ name: "", tag: "" });
   };
@@ -46,25 +59,73 @@ class Form extends Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
+        <label htmlFor={this.nameInputId}>
           Name{" "}
           <input
+            id={this.nameInputId}
             type="text"
             name="name"
             value={this.state.name}
             onChange={this.handleChange}
           />
         </label>
-        <label>
+        <label htmlFor={this.tagInputId}>
           Tag{" "}
           <input
+            id={this.tagInputId}
             type="text"
             name="tag"
             value={this.state.tag}
             onChange={this.handleChange}
           />
         </label>
-        <button tupe="submit">Add</button>
+
+        <p>Your level:</p>
+        <label>
+          <input
+            type="radio"
+            name="experience"
+            value="junior"
+            onChange={this.handleChange}
+            checked={this.state.experience === "junior"}
+          />
+          Junior
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="experience"
+            value="middle"
+            onChange={this.handleChange}
+            checked={this.state.experience === "middle"}
+          />
+          Middle
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="experience"
+            value="senior"
+            onChange={this.handleChange}
+            checked={this.state.experience === "senior"}
+          />
+          Senior
+        </label>
+
+        <label>
+          {/* checked={this.state.licence} буквально зависит от значения в стейте */}
+          <input
+            type="checkbox"
+            name="licence"
+            checked={this.state.licence}
+            onChange={this.handleLicenceChange}
+          />{" "}
+          Agree with the condition
+        </label>
+
+        <button type="submit" disabled={!this.state.licence}>
+          Send
+        </button>
       </form>
     );
   }
